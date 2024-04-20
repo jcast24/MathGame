b92ea08cd0b12db7b75c8e762cbe476a0c0ca383
Jesus Castro <castrojesus39@gmail.com>
2024-04-19 19:41:39 -0700

    nothing changed

--- Program.cs
+++ Program.cs
@@ -4,11 +4,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the Math game!");
-        // Console.WriteLine("How many games would you like to play?: ");
-
-        // var input = int.Parse(Console.ReadLine());
         var menu = new Menu();
         menu.MenuSystem();
-
     }
 }
