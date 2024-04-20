6e93e786eaf104c1c0f974ba58ff9d223973f7bd
Jesus Castro <castrojesus39@gmail.com>
2024-04-19 19:41:26 -0700

    added validation when user types wrong value when asking for a int

--- Menu.cs
+++ Menu.cs
@@ -3,9 +3,26 @@ internal class Menu
 {
     internal int AskUser()
     {
-        Console.WriteLine("Please enter the amount of questions you would like to ask: ");
-        int input = int.Parse(Console.ReadLine());
-        return input;
+        // Console.WriteLine("Please enter the amount of questions you would like to ask: ");
+        // int input = int.Parse(Console.ReadLine());
+        // return input;
+        int number;
+        bool isValid = false;
+
+        do
+        {
+            Console.WriteLine("Please enter the amount of questions you would like to be asked: ");
+            var input = Console.ReadLine();
+
+            isValid = int.TryParse(input, out number);
+
+            if (!isValid)
+            {
+                Console.WriteLine("Invalid input please try again: ");
+            }
+        } while (!isValid);
+
+        return number;
     }
     
     internal void MenuSystem()
@@ -27,7 +44,7 @@ internal class Menu
             Console.WriteLine("(r) Random Game!");
             Console.WriteLine("(q) Quit!");
 
-            string input = Console.ReadLine();
+            string? input = Console.ReadLine();
 
             switch (input.Trim().ToLower())
             {
