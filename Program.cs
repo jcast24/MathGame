2ec4e695f78ad6d28a97a2ab834fd68143152b20
Jesus Castro <castrojesus39@gmail.com>
2024-04-19 19:15:33 -0700

    removed asking user number of games and created a new method in Menu.cs

--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the Math game!");
-        Console.WriteLine("How many games would you like to play?: ");
+        // Console.WriteLine("How many games would you like to play?: ");
 
-        var input = int.Parse(Console.ReadLine());
+        // var input = int.Parse(Console.ReadLine());
         var menu = new Menu();
-        menu.MenuSystem(input);
+        menu.MenuSystem();
 
     }
 }
